c3087dc0513fcdc2e193f4a92bc52df1ef3d6bbe
paulb <pabennett@hotmail.co.uk>
2024-01-31 22:22:11 +0000

    [01] Challenge BMI Comparison

---
+++ 01_fundamentals/challenge1.js
@@ -0,0 +1,20 @@
+//BMI comparison 
+console.log("Challenge 1");
+
+compareBmi(78, 1.69, 92, 1.95);
+compareBmi(95, 1.88, 85, 1.76);
+
+function compareBmi(markWeight, markHeight, johnWeight, johnHeight){
+    let markBmi = getBmi(markWeight, markHeight);
+    let johnBmi = getBmi(johnWeight, johnHeight);
+
+    if(markBmi === johnBmi)     console.log("They have the same BMI");
+    else if(markBmi > johnBmi)  console.log("Mark has the greater BMI");
+    else                        console.log("John has the greater BMI");
+}
+
+//weight in kg, height in m
+function getBmi(weight, height){
+    //BMI = mass / (height * height)
+    return weight / (height ** 2);
+}
