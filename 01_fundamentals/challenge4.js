022a597d5c42dc36e81e76aef7087d451bcee8d0
paulb <pabennett@hotmail.co.uk>
2024-01-31 22:30:31 +0000

    [01] Challenge Tip Calculation

---
+++ 01_fundamentals/challenge4.js
@@ -0,0 +1,19 @@
+//Tip calculation
+
+stateTotal(275);
+stateTotal(40);
+stateTotal(430);
+
+function stateTotal(bill){
+    const theTip = getTip(bill);
+    const total = bill + theTip;
+
+    console.log(`That is ${bill} plus ${theTip}`)
+    console.log(`The total is ${total}`);
+}
+
+function getTip(theBill){
+    const theTipPercentage = theBill >= 50 && theBill <= 300 ? 0.15 : 0.20;
+    const theTipCalc       = theBill * theTipPercentage;
+    return Number(theTipCalc.toFixed(2));
+}
