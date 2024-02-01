702422a6e7d236ddfeecc1e04a439fc42ba1ebd9
paulb <pabennett@hotmail.co.uk>
2024-02-01 17:39:45 +0000

    [02] Challenge BMI Comparison

---
+++ 02_fundamentals/challenge3.js
@@ -0,0 +1,42 @@
+"use strict";
+//BMI comparison
+console.log("Challenge 2");
+
+let mark = {
+    fullName: `Mark Miller`,
+    mass: 78,
+    height: 1.69,
+    calcBmi: function(){
+        let bmi = Number((this.mass/(this.height**2)).toFixed(2));
+        this.bmi = bmi;
+        return bmi;
+    },
+    desc: function(){
+        console.log(`${this.fullName}'s weighs ${this.mass}kg and is ${this.height}m tall with a BMI of ${this.bmi}`);
+    }
+}
+
+let john = {
+    fullName: `John Smith`,
+    mass: 92,
+    height: 1.95,
+    calcBmi: function(){
+        let bmi = Number((this.mass/(this.height**2)).toFixed(2));
+        this.bmi = bmi;
+        return bmi;
+    },
+    desc: function(){
+        console.log(`${this.fullName}'s weighs ${this.mass}kg and is ${this.height}m tall with a BMI of ${this.bmi}`);
+    }
+}
+
+//Run  calculations first
+mark.calcBmi();
+john.calcBmi();
+
+mark.desc();
+john.desc();
+
+if(mark.bmi > john.bmi)         console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher that ${john.fullName}'s (${john.bmi})`);
+else if(john.bmi > mark.bmi)    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher that ${mark.fullName}'s (${mark.bmi})`);
+else                            console.log(`Mark and John have the same BMI`);
