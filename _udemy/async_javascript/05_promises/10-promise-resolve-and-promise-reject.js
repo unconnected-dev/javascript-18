11e06be338e2668b545d826ff7fa0b6a3c849c5f
paulb <pabennett@hotmail.co.uk>
2024-08-22 12:54:31 +0100

    [05] Async JavaScript
    Promise resolve and promise reject

---
+++ _udemy/async_javascript/05_promises/10-promise-resolve-and-promise-reject.js
@@ -0,0 +1,45 @@
+"use strict";
+
+
+//This function accepts a promise as an argument and logs its resolved value
+//to the console
+function logToConsole(somePromise){
+    somePromise.then(value => console.log(`${value}`));
+}
+
+
+//Create a promise and pass this promise to the logToConsole function
+//The promise is resolved with the value 'Hello'
+const somePromise = new Promise(
+    (resolve, reject) => resolve('Hello')
+);
+logToConsole(somePromise);
+
+
+
+//Example of passing a non-promise value to logToConsole
+//Uncommenting the following line will throw an error because logToConsole
+//expects a promise, not a string
+
+//const value = 'string';
+//logToConsole(value);
+
+
+
+//Promise.resolve
+//Creates a promise that is resolved with the given value
+//If the value is already a promise, Promise.resolve returns it unchanged
+//This is useful for wrapping non-promise values in a promise
+const promisifiedValue = Promise.resolve(value);
+logToConsole(promisifiedValue);
+
+
+//Promise.reject
+//Creates a promise that is rejected with the given reason
+//You can pass any value as the reason, but it is recommended to use Error
+//objects for better error handling
+const rejectedValue = Promise.reject(value);
+logToConsole(rejectedValue);
+
+const rejectedValueError = Promise.reject(new Error('an error'));
+logToConsole(rejectedValueError);
