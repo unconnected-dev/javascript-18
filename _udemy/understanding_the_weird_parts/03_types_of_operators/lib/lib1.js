67036cf24a492cb2370195d404702617951ecedc
paulb <pabennett@hotmail.co.uk>
2024-09-28 17:23:39 +0100

    [03] Understanding The Weird Parts

---
+++ _udemy/understanding_the_weird_parts/03_types_of_operators/lib/lib1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+
+//Check if the global variable 'libraryName' is already defined before assigning it
+//This prevents overwriting if another script has already set it
+var libraryName = window.libraryName || "Lib 1";
