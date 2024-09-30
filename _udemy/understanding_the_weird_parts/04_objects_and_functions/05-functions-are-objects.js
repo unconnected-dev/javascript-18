956d5df350cdeb01873d5a412e2fad5633f709e9
paulb <pabennett@hotmail.co.uk>
2024-09-30 19:26:00 +0100

    [04] Understanding The Weird Parts

---
+++ _udemy/understanding_the_weird_parts/04_objects_and_functions/05-functions-are-objects.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+
+//First-Class Functions in JavaScript
+//In JavaScript, functions are treated as first-class citizens. This means
+//functions are objects, and you can do anything with them that you can do
+//with other objects, such as:
+//  - Assign them to variables
+//  - Pass them as arguments to other functions
+//  - Return them from other functions
+//  - Create them dynamically
+
+
+//Functions are a special type of object.
+//In addition to executing code, they can have properties and methods attached
+//to them, just like any other object
+
+//Properties of a function:
+//  - Name property: A function can have a name, but it isn't required 
+//    (anonymous functions have no name)
+//  - Code property: The actual code inside the function is stored in a 
+//    property, which is invokable
+
+
+//The code inside the function is just one property of the function object. The
+//function itself is a full object, with potential for other properties
+
+
+function greet(){
+    console.log(`Hi`);
+}
+
+//Adding a custom property to the function object:
+greet.language = `english`;
+
+
+//Outputs the function's code (since functions are objects):
+console.log(`${greet}`); 
+
+//Accessing the custom property added to the function:
+console.log(`${greet.language}`);
