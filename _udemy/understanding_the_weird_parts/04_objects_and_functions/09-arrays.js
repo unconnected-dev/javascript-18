591b6dc1e76f82e8ff2c6f1d1c72fafbf3021052
paulb <pabennett@hotmail.co.uk>
2024-10-01 15:40:34 +0100

    [04] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/04_objects_and_functions/09-arrays.js
+++ _udemy/understanding_the_weird_parts/04_objects_and_functions/09-arrays.js
@@ -5,7 +5,7 @@
 //An array is a collection that can hold multiple values, even of different 
 //types
 var arr = [1, 2, 3];
-console.log(`${arr[1]}`);
+console.log(`arr[1]: ${arr[1]}`);
 
 
 //In many languages, arrays typically store values of the same type. However,
@@ -23,15 +23,15 @@ var arr2 = [
     },
     function(name){                         //Function
         var greeting = `Hello`;
-        console.log(`${greeting} ${name}`);
+        console.log(`greeting name: ${greeting} ${name}`);
     }];
 
     
 //Accessing and displaying different elements from the array
-console.log(`${arr2[0]}`);
-console.log(`${arr2[1]}`);
-console.log(`${arr2[2]}`);
-console.log(`${arr2[3]}`);
-console.log(`${arr2[3].name}`);
-console.log(`${arr2[4]}`);
+console.log(`arr2[0]: ${arr2[0]}`);
+console.log(`arr2[1]: ${arr2[1]}`);
+console.log(`arr2[2]: ${arr2[2]}`);
+console.log(`arr2[3]: ${arr2[3]}`);
+console.log(`arr2[3].name: ${arr2[3].name}`);
+console.log(`arr2[4]: ${arr2[4]}`);
 arr2[4](`ted`);
