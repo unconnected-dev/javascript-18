02126f97283028aca24dda0a6ed83374b319fe9c
paulb <pabennett@hotmail.co.uk>
2024-10-02 03:40:48 +0100

    [04] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/04_objects_and_functions/07-by-value-vs-by-reference.js
+++ _udemy/understanding_the_weird_parts/04_objects_and_functions/07-by-value-vs-by-reference.js
@@ -2,64 +2,64 @@
 
 
 
-//Mutate:    To change or modify something
-//Immutable: Something that cannot be changed
+// Mutate:    To change or modify something
+// Immutable: Something that cannot be changed
 
 
 // Primitives and References in JavaScript:
 
-//When dealing with primitive values (like numbers, strings, etc.), variables
-//store the actual value in memory
+// When dealing with primitive values (like numbers, strings, etc.), variables
+// store the actual value in memory
 var a = 1;
 
 
-//If a variable pointing to a primitive is copied to another variable, a new
-//copy of that value is made in a different location in memory. This is called
-//"pass by value"
+// If a variable pointing to a primitive is copied to another variable, a new
+// copy of that value is made in a different location in memory. This is 
+// called "pass by value"
 var b = a;
 
-//Mutating `a` does not affect `b` because they reference different locations 
-//in memory
+// Mutating `a` does not affect `b` because they reference different locations 
+// in memory
 
 
-//Objects behave differently. When you assign an object to a variable, the 
-//variable stores a reference (address) to the object's location in memory, 
-//not the object itself
+// Objects behave differently. When you assign an object to a variable, the 
+// variable stores a reference (address) to the object's location in memory, 
+// not the object itself
 var c = {};
 
 
-//When assigning one object reference to another variable, both variables point
-//to the same object in memory. This is called "pass by reference"
+// When assigning one object reference to another variable, both variables 
+// point to the same object in memory. This is called "pass by reference"
 var d = c;
 
 
 
-//Example of "pass by reference" (with objects):
+// Example of "pass by reference" (with objects):
 var x = { greeting: `hi` };
-var y = x;  //`y` references the same object as `x`
+var y = x;  // `y` references the same object as `x`
 
-//Mutating the object through `x`
+// Mutating the object through `x`
 x.greeting = `Hello`;
 console.log(`x.greeting: ${x.greeting}`);
 console.log(`y.greeting: ${y.greeting}`);
 
 
 
-//Pass by reference when passing objects as function arguments:
+// Pass by reference when passing objects as function arguments:
 function changeGeeting(obj){
 
-    //Mutates the original object
+    // Mutates the original object
     obj.greeting = `Hola`;  
 }
 
-//Passes `x` (and `y`, as they reference the same object) to the function
+// Passes `x` (and `y`, as they reference the same object) to the function
 changeGeeting(x);
 console.log(`x.greeting: ${x.greeting}`);
 console.log(`y.greeting: ${y.greeting}`);
 
 
-//Creating a new object with the `=` operator sets up a new memory space 
-//(new address). `y` is now pointing to a different object than `x`
+// Creating a new object with the `=` operator sets up a new memory space 
+// (new address). `y` is now pointing to a different object than `x`
 y = { greeting: `howdy` };
 console.log(`x.greeting: ${x.greeting}`);
 console.log(`y.greeting: ${y.greeting}`);
