e3c87a66212fae5c456ced6ce986891783373a40
paulb <pabennett@hotmail.co.uk>
2024-10-02 03:11:26 +0100

    [03] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/03_types_of_operators/06-default-values.js
+++ _udemy/understanding_the_weird_parts/03_types_of_operators/06-default-values.js
@@ -2,7 +2,7 @@
 
 
 
-//Function Parameter Handling in JavaScript
+// Function Parameter Handling in JavaScript
 
 function greet(name){
     console.log(`Hello ${name}`);
@@ -10,26 +10,26 @@ function greet(name){
 
 if(false){
     greet(`Tony`);
-    //Output: `Hello Tony
+    // Output: Hello Tony
 }
 
 
-//In JavaScript, functions are flexible with arguments. If no argument is passed
-//the function does not throw an error like in many other languages. Instead,
-//the parameter `name` is implicitly set to `undefined`
+// In JavaScript, functions are flexible with arguments. If no argument is passed
+// the function does not throw an error like in many other languages. Instead,
+// the parameter `name` is implicitly set to `undefined`
 if(false){
     greet();
-    //Output: `Hello undefined`
+    // Output: Hello undefined
 }
 
 
 
-//Using Default Values with Logical OR (||)
+// Using Default Values with Logical OR (||)
 
-//The `||` operator in JavaScript returns the first "truthy" value it encounters
-//If the first operand is falsy (e.g., `undefined`, `null`, `0`, `""`), it 
-//returns the second operand. This makes it useful for setting default values 
-//in functions
+// The `||` operator in JavaScript returns the first "truthy" value it encounters.
+// If the first operand is falsy (e.g., `undefined`, `null`, `0`, `""`), it 
+// returns the second operand. This makes it useful for setting default values 
+// in functions.
 
 function greet2(name){
     name = name || `<Your name here>`;
@@ -38,27 +38,27 @@ function greet2(name){
 
 if(false){
     greet2();
-    //Output: Hello <Your name here>
+    // Output: Hello <Your name here>
 }
 
 if(false){
     greet2('Tony');
-    //Output: Hello Tony
+    // Output: Hello Tony
 }
 
 
 
 if(false){
     console.log(`"hi" || "hello": ${"hi" || "hello"}`);
-    //Output: hi
+    // Output: hi
 }
 
 if(false){
     console.log(`0 || 1: ${0 || 1}`);
-    //Output: 1
+    // Output: 1
 }
 
 if(false){
     console.log(`"" || "hello": ${"" || "hello"}`);
-    //Output: hello
+    // Output: hello
 }
