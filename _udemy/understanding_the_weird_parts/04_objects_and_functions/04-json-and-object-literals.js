4e3ce2b02941ee9653b5333605a50582516c55f2
paulb <pabennett@hotmail.co.uk>
2024-10-02 03:25:31 +0100

    [04] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/04_objects_and_functions/04-json-and-object-literals.js
+++ _udemy/understanding_the_weird_parts/04_objects_and_functions/04-json-and-object-literals.js
@@ -2,27 +2,27 @@
 
 
 
-//JavaScript Object Notation (JSON)
-//JSON is inspired by JavaScript's object literal syntax but is used for data
-//interchange between systems
+// JavaScript Object Notation (JSON)
+// JSON is inspired by JavaScript's object literal syntax but is used for 
+// data interchange between systems.
 
 
 var objectLiteral = {
     firstname: `Mary`,
     isAProgrammer: true
-}
+};
 
 console.log(`objectLiteral: ${objectLiteral}`);
 
 
-//Differences between JSON and object literals:
+// Differences between JSON and object literals:
 //  - Property names in JSON must be wrapped in double quotes (""), whereas
-//    in object literals, it's optional
-//  - JSON is a subset of JavaScript object literal syntax, meaning all valid
-//    JSON is valid object literal syntax, but not all object literal syntax 
-//    is valid JSON
+//    in object literals, it's optional.
+//  - JSON is a subset of JavaScript object literal syntax, meaning all 
+//    valid JSON is valid object literal syntax, but not all object literal 
+//    syntax is valid JSON.
 
-//Example of JSON:
+// Example of JSON:
 // {
 //     "firstname": "Mary",
 //     "isAProgrammer": true
@@ -30,19 +30,19 @@ console.log(`objectLiteral: ${objectLiteral}`);
 
 
 
-//JSON and JavaScript:
-//JSON isn't a part of JavaScript, but since JSON is so widely used and 
-//JavaScript works well with it, JavaScript includes built-in functionality
-//for converting between JSON and objects
+// JSON and JavaScript:
+// JSON isn't a part of JavaScript, but since JSON is so widely used and 
+// JavaScript works well with it, JavaScript includes built-in functionality
+// for converting between JSON and objects.
 
-//Converting an object to JSON:
-//JSON.stringify() turns an object into a JSON string, useful for sending 
-//data to a server or saving it
+// Converting an object to JSON:
+// JSON.stringify() turns an object into a JSON string, useful for sending 
+// data to a server or saving it.
 console.log(JSON.stringify(objectLiteral));  
 
 
 // Converting JSON back to an object:
 // JSON.parse() turns a JSON string back into a JavaScript object, useful
-//when receiving data from a server
+// when receiving data from a server.
 var jsonValue = JSON.parse(`{"firstname": "Mary", "isAProgrammer": true}`);
 console.log(`jsonValue: ${jsonValue}`);
