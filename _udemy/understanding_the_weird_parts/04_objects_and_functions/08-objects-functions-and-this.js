e3de1d1ef1e3069d6a86989471eb5a91dfa329e1
paulb <pabennett@hotmail.co.uk>
2024-10-02 03:44:01 +0100

    [04] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/04_objects_and_functions/08-objects-functions-and-this.js
+++ _udemy/understanding_the_weird_parts/04_objects_and_functions/08-objects-functions-and-this.js
@@ -2,8 +2,8 @@
 
 
 
-//When a function is invoked, a new execution context is created
-//Each execution context consists of:
+// When a function is invoked, a new execution context is created
+// Each execution context consists of:
 //  - A variable environment 
 //  - A reference to the outer environment
 //  - The `this` keyword, which refers to an object. The value of `this`
@@ -11,22 +11,22 @@
 
 
 
-//Global context: 
-//In the global scope, `this` refers to the global object
-//In strict mode, this will be `undefined`
+// Global context: 
+// In the global scope, `this` refers to the global object
+// In strict mode, this will be `undefined`
 console.log(`this: ${this}`);
 
 
 
-//In a function, `this` behaves differently:
+// In a function, `this` behaves differently:
 function a(){
 
-    //In strict mode, `this` inside a function does not refer to the global
-    //object, instead, it's `undefined` because strict mode prevents accidental
-    //reference to the global context
+    // In strict mode, `this` inside a function does not refer to the global
+    // object, instead, it's `undefined` because strict mode prevents 
+    // accidental reference to the global context
     console.log(`this: ${this}`);
 
-    //Line below would throw an error because `this` is undefined in strict mode:
+    // Line below would throw an error because `this` is undefined in strict mode:
     // this.newVariable = `Hello`;
 }
 a();
@@ -34,49 +34,49 @@ a();
 
 var b = function(){
     
-    //Similar to function `a`, in strict mode, `this` inside function `b` is 
-    //`undefined`
+    // Similar to function `a`, in strict mode, `this` inside function `b` is 
+    // `undefined`
     console.log(`this: ${this}`);
 }
 b();
 
-//Accessing `this.newVariable` will return `undefined` since `this` in the 
-//global scope refers to the global object (or `undefined` in strict mode)
+// Accessing `this.newVariable` will return `undefined` since `this` in the 
+// global scope refers to the global object (or `undefined` in strict mode)
 console.log(`this.newVariable: ${this.newVariable}`); 
 
 
 
-//When `this` is used inside an object method, it refers to the object itself
+// When `this` is used inside an object method, it refers to the object itself
 var c = {
     name: `The c object`,
     log: function(){
 
-        //In this case, `this` refers to the object `c` because `log` is a 
-        //method of `c`
+        // In this case, `this` refers to the object `c` because `log` is a 
+        // method of `c`
         console.log(`this: ${this}`);
-        //Outputs: "The c object"
+        // Outputs: "The c object"
         console.log(`this.name: ${this.name}`);
 
 
-        //Problem with `this` inside inner functions:
-        //`this` inside an inner function does not refer to the outer object
-        //(`c`), but instead would be `undefined` in strict mode or point to
-        //the global object in non-strict mode
+        // Problem with `this` inside inner functions:
+        // `this` inside an inner function does not refer to the outer object
+        // (`c`), but instead would be `undefined` in strict mode or point to
+        // the global object in non-strict mode
 
-        //var setName = function(newName) {
-        //    this.name = newName; //`this` doesn't refer to `c` so will error
-        //};
-        //setName("Bob");
+        // var setName = function(newName) {
+        //     this.name = newName; // `this` doesn't refer to `c` so will error
+        // };
+        // setName("Bob");
         
 
-        //Workaround using a variable (`self`) to maintain the correct 
-        //reference to `this` through the scope chain. It helps avoid the issue 
-        //of thinking what `this` refers to
+        // Workaround using a variable (`self`) to maintain the correct 
+        // reference to `this` through the scope chain. It helps avoid the issue 
+        // of thinking what `this` refers to
 
-        //`self` is not a reference to `c`
+        // `self` is not a reference to `c`
         var self = this;
         var setname2 = function(name){
-            //uses `self` to modigy `c.name`
+            // uses `self` to modify `c.name`
             self.name = name;
         }
         setname2(`bob`);
