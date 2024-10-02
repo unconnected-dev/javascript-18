a81fb305d391a942afa9685fc1bdf24e0f66bbed
paulb <pabennett@hotmail.co.uk>
2024-10-02 03:05:18 +0100

    [03] Understanding The Weird Parts

--- _udemy/understanding_the_weird_parts/03_types_of_operators/03-coercion.js
+++ _udemy/understanding_the_weird_parts/03_types_of_operators/03-coercion.js
@@ -1,32 +1,32 @@
 "use strict";
 
 
-//Coercion
-//Coercion is the process of converting a value from one data type to another
-//This happens frequently in JavaScript because it is dynamically typed, meaning
-//variables can hold values of any type without explicit declarations
+// Coercion
+// Coercion is the process of converting a value from one data type to another.
+// This happens frequently in JavaScript because it is dynamically typed, meaning
+// variables can hold values of any type without explicit declarations.
 
 
-//Both values are strings, so JavaScript concatenates them
+// Both values are strings, so JavaScript concatenates them.
 var a = 'hello' + 'world';
 console.log(`a: ${a}`);
 
-//Here JavaScript coerces the number 1 into a string and then cocantenates it with
-//'2'. This results in the string being '12', not the number 3
+// Here JavaScript coerces the number 1 into a string and then concatenates it 
+// with '2'. This results in the string being '12', not the number 3.
 var b = 1 + '2';
 console.log(`b: ${b}`);
 
 
 
-//JavaScript automatically performs type coercion when it encounters mixed types 
-//in operations
+// JavaScript automatically performs type coercion when it encounters mixed types 
+// in operations.
 
-//JavaScript’s dynamic typing means it doesn’t throw errors in such cases, unlike
-//statically typed languages (like Java or C#) where type mismatches would cause
-//errors
+// JavaScript’s dynamic typing means it doesn’t throw errors in such cases, unlike
+// statically typed languages (like Java or C#) where type mismatches would cause
+// errors.
 
-//This is why JavaScript tends to convert values to what it thinks the 
-//programmer intended
+// This is why JavaScript tends to convert values to what it thinks the 
+// programmer intended.
 
-//However, this implicit coercion can sometimes lead to unexpected results, so 
-//it’s important to be aware of when and how coercion occurs
+// However, this implicit coercion can sometimes lead to unexpected results, so 
+// it’s important to be aware of when and how coercion occurs.
