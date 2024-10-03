d0ca3baa7335a3ae7f6f7f4f4c1be34697051ca2
paulb <pabennett@hotmail.co.uk>
2024-10-03 17:23:37 +0100

    [05] Understanding The Weird Parts

---
+++ _udemy/understanding_the_weird_parts/05_object_orientated_javascript/01-classical-vs-prototypal-inheritance.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+
+// Classical vs. Prototypal Inheritance
+
+// Inheritance:
+// - Refers to one object gaining access to the properties and methods of 
+//   another object.
+
+
+// Classical Inheritance:
+// - Commonly seen in languages like Java, C++, etc.
+// - Requires creating classes and using mechanisms like inheritance chains.
+// - Uses concepts like:
+//     - friend
+//     - protected
+//     - private
+//     - interface
+// - Can be verbose and strict with fixed structure and rules.
+
+
+
+// Prototypal Inheritance (JavaScript uses this):
+// - More flexible and simpler compared to classical inheritance.
+// - Objects inherit directly from other objects (no need for classes).
+// - Key features:
+//     - Flexible: Allows dynamic modifications to objects at runtime.
+//     - Extensible: New properties and methods can be added easily.
+//     - Easy to understand: Avoids the complexity of classical inheritance,
+//       focusing on object-to-object delegation.
