876e1b24f51c8044599a88ab82aa388a4a262b42
paulb <pabennett@hotmail.co.uk>
2024-10-09 20:48:17 +0100

    [07] ES6 And Beyond

--- _udemy/understanding_es6_and_beyond/07_iterators_and_iterables/01-for-of-for-in.js
+++ _udemy/understanding_es6_and_beyond/07_iterators_and_iterables/01-for-of-for-in.js
@@ -4,37 +4,37 @@
 
 // Enumerable
 // - A property is considered enumerable if it appears during a loop over the
-//   properties of an object (e.g., using for...in)
+//   properties of an object (e.g., using for...in).
 // - By default, properties created via object literals or using the 
 //   `Object.defineProperty` method with the `enumerable` flag set to true will 
-//   be enumerable
+//   be enumerable.
 
 let names = ["Tony", "Anthony", "Alicea"];
 
 
-//for...of Loop
-//Used to iterate over the values of an iterable object, such as 
-//arrays, strings, maps, sets, etc
-////Typically used when you need to access the values of an array 
-//or other iterable object
+// for...of Loop
+// Used to iterate over the values of an iterable object, such as 
+// arrays, strings, maps, sets, etc.
+// Typically used when you need to access the values of an array 
+// or other iterable object.
 for(const name of names){
-    console.log(`name: ${name}`);//Outputs each value in the array
+    console.log(`name: ${name}`);// Outputs each value in the array
 }
 
-//for...in Loop
-//Used to iterate over the enumerable properties (keys) of an object
-//Typically used to iterate over the keys of an object or indices
-//of an array
+// for...in Loop
+// Used to iterate over the enumerable properties (keys) of an object.
+// Typically used to iterate over the keys of an object or indices
+// of an array.
 for(const name in names){
-    console.log(name);//Outputs each index in the array
+    console.log(name);// Outputs each index in the array
 }
 
 // - The for...in loop iterates over all enumerable properties of an object,
-//   including those inherited from the prototype chain
+//   including those inherited from the prototype chain.
 // - This loop returns keys as strings, even for arrays where the keys are
-//   numeric indices
+//   numeric indices.
 // - It is not suitable for arrays when the order of iteration is important,
-//   as the order is not guaranteed
+//   as the order is not guaranteed.
 
 
 
@@ -57,10 +57,12 @@ for(const prop in dog){
     console.log(prop);// Outputs the keys 'name' and 'sound'
 }
 
-// - The for...in loop will iterate over both the instance properties (like 'sound') 
-//   and any properties inherited from the prototype (like 'name' from Animal).
-// - This means you might see properties from parent classes or from the object's 
-//   prototype chain, not just those directly defined on the instance
+// - The for...in loop will iterate over both the instance properties 
+//   (like 'sound') and any properties inherited from the prototype 
+//   (like 'name' from Animal).
+// - This means you might see properties from parent classes or from the 
+//   object's prototype chain, not just those directly defined on the 
+//   instance.
 
 
 
@@ -71,13 +73,14 @@ let dog2 = {
     }
 }
 
-//Prototype Chain
-// - The for...in loop not only iterates over an object's own enumerable properties 
-//   but also over those properties down its prototype chain
-// - This can result in unexpected properties being iterated over if the object's
-//   prototype has enumerable properties
-// - Iterating over the prototype chain can lead to unexpected behavior if not handled
-//   carefully, as it may include properties you do not intend to access
+// Prototype Chain
+// - The for...in loop not only iterates over an object's own enumerable
+//   properties  but also over those properties down its prototype chain.
+// - This can result in unexpected properties being iterated over if the 
+//   object's prototype has enumerable properties.
+// - Iterating over the prototype chain can lead to unexpected behavior 
+//   if not handled carefully, as it may include properties you do not intend
+//   to access.
 
 dog2.__proto__ = {
     residence: "Earth"
@@ -88,16 +91,18 @@ for(const prop in dog2){
 }
 
 
-//Checking if a Property is Enumerable
-// - The `propertyIsEnumerable()` method can be used to check if a specific property is
-//   enumerable on an object
+// Checking if a Property is Enumerable
+// - The `propertyIsEnumerable()` method can be used to check if a specific 
+//   property is enumerable on an object.
 console.log(dog.propertyIsEnumerable("sound")); //Outputs: true
 
 
 
-//Error Case for for...of
-// - Non-iterable objects (like regular objects) will cause an error if used with for...of
-// - Attempting to use a for...of loop with a non-iterable object will throw a `TypeError`
+// Error Case for for...of
+// - Non-iterable objects (like regular objects) will cause an error if used 
+//   with for...of.
+// - Attempting to use a for...of loop with a non-iterable object will throw 
+//   a `TypeError`.
 try {
     for(const prop of dog){
         console.log(prop); 
@@ -105,18 +110,20 @@ try {
 } catch(e) {
     console.log(e.message); // Outputs: dog is not iterable
 }
-// - Ensure that the object you use with for...of is iterable (e.g., arrays, strings, maps)
-// - If you need to iterate over properties of a non-iterable object, consider using 
-//   for...in instead
+// - Ensure that the object you use with for...of is iterable 
+//   (e.g., arrays, strings, maps).
+// - If you need to iterate over properties of a non-iterable object, consider
+//   using for...in instead.
 
 
 
-//for...of:
-// - Iterates over values of an iterable (e.g., array, string)
-// - Suitable for iterating arrays, strings, and other iterable objects
-// - Does not work with non-iterable objects (throws an error)
+// for...of:
+// - Iterates over values of an iterable (e.g., array, string).
+// - Suitable for iterating arrays, strings, and other iterable objects.
+// - Does not work with non-iterable objects (throws an error).
 
-//for...in:
-// - Iterates over enumerable properties (keys) of an object
-// - Suitable for iterating over the properties of objects and the indices of arrays
-// - Iterates over properties in the object's prototype chain as well
+// for...in:
+// - Iterates over enumerable properties (keys) of an object.
+// - Suitable for iterating over the properties of objects and the indices of 
+//   arrays.
+// - Iterates over properties in the object's prototype chain as well.
--- _udemy/understanding_es6_and_beyond/07_iterators_and_iterables/02-iterators.js
+++ _udemy/understanding_es6_and_beyond/07_iterators_and_iterables/02-iterators.js
@@ -1,38 +1,37 @@
 "use strict";
 
 
+// Protocols:
+// A protocol defines a set of rules or conventions for how to exchange
+// information.
 
-//Protocols:
-//A protocol defines a set of rules or conventions for how to exchange
-//information
+// Iteration Protocols:
+// The iteration protocol defines the specific set of rules that objects
+// must follow to be iterable, meaning they can be iterated over using
+// constructs like `for...of` loops.
 
-//Iteration Protocols:
-//The iteration protocol defines the specific set of rules that objects
-//must follow to be iterable, meaning they can be iterated over using
-//constructs like `for...of` loops
+// "Iterate Over":
+// To "iterate over" means to move from value to value within a set or 
+// list of values.
 
-//"Iterate Over":
-//To "iterate over" means to move from value to value within a set or 
-//list of values
-
-//Iterable:
-//An object is considered iterable if it implements the `[Symbol.iterator]`
-//method
-//This method must return an iterator, which is an object that adheres to 
-//the iterator protocol
+// Iterable:
+// An object is considered iterable if it implements the `[Symbol.iterator]`
+// method.
+// This method must return an iterator, which is an object that adheres to 
+// the iterator protocol.
 
 
-//Example of Creating an Iterable Object:
-//The `numbers` object can become iterable if it has a method that returns
-//an iterator which conforms to the iteration protocol
+// Example of Creating an Iterable Object:
+// The `numbers` object can become iterable if it has a method that returns
+// an iterator which conforms to the iteration protocol.
 let numbers = {
 
     getIterator: function(start, end){
         let nextValue = start;
         return {
             
-            //The `next` method is part of the iterator protocol
-            //It returns an object with two properties: 
+            // The `next` method is part of the iterator protocol
+            // It returns an object with two properties: 
             // -`value` (the current value)
             // - `done` (a boolean indicating whether the iteration is complete)
             next: () => {
@@ -56,21 +55,21 @@ console.log(iterator.next());
 
 
 
-//Using the `[Symbol.iterator]` method to make an object iterable:
+// Using the `[Symbol.iterator]` method to make an object iterable:
 let numbers2 = {
 
     type: "Integers",
     decimal: false,
 
-    //The `[Symbol.iterator]` method makes the `numbers2` object iterable
+    // The `[Symbol.iterator]` method makes the `numbers2` object iterable
     [Symbol.iterator]: function(){
         const start = 0;
         const end = 5;
         let nextValue = start;
         return {
 
-            //The iterator must have a `next` method that returns an object 
-            //with `value` and `done` properties
+            // The iterator must have a `next` method that returns an object 
+            // with `value` and `done` properties
             next: () => {
                 if(nextValue <= end){
                     let nextObj = { value: nextValue, done: false };
@@ -85,7 +84,7 @@ let numbers2 = {
 
 
 if(false){
-    //This `for...of` loop works because `numbers2` is iterable
+    // This `for...of` loop works because `numbers2` is iterable
     for(const num of numbers2){
         console.log(`${num}`);
     }
@@ -93,10 +92,10 @@ if(false){
 
 
 if(false){
-    //Properties defined using a Symbol (like `[Symbol.iterator]`) are not
-    //enumerable, meaning they are not included when iterating over properties
-    //with `for...in`
-    //The loop below will only log the enumerable properties of `numbers2`
+    // Properties defined using a Symbol (like `[Symbol.iterator]`) are not
+    // enumerable, meaning they are not included when iterating over properties
+    // with `for...in`.
+    // The loop below will only log the enumerable properties of `numbers2`.
     for(const prop in numbers2){
         console.log(`${prop}`);
     }
@@ -104,7 +103,7 @@ if(false){
 
 
 if(false){
-    //This manually iterates over `numbers2` using its iterator
+    // This manually iterates over `numbers2` using its iterator
     let iterator2 = numbers[Symbol.iterator]();
     let next = iterator2.next();
 
@@ -115,17 +114,20 @@ if(false){
 }
 
 
-//Protocols:
-// - Define rules for information exchange
+// Protocols:
+// - Define rules for information exchange.
 
-//Iteration Protocols:
-// - Allow objects to be iterable, typically using for...of loops
+// Iteration Protocols:
+// - Allow objects to be iterable, typically using for...of loops.
 
-//Iterables:
-// - Objects that implement the [Symbol.iterator] method and return an iterator
+// Iterables:
+// - Objects that implement the [Symbol.iterator] method and return an 
+//   iterator.
 
-//Iterators:
-// - Objects with a next() method that returns an object containing value and done
+// Iterators:
+// - Objects with a next() method that returns an object containing value and
+//   done.
 
-//Symbol Properties:
-// - Properties like [Symbol.iterator] are not enumerable and won’t appear in for...in loops
+// Symbol Properties:
+// - Properties like [Symbol.iterator] are not enumerable and won’t appear in 
+//   for...in loops.
