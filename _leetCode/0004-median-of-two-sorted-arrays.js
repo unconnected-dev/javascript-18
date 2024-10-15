5b7019c51280409c9419f40b2d4b27fdf91b7fff
paulb <pabennett@hotmail.co.uk>
2024-10-15 14:16:33 +0100

    [LeetCode 0004] +

--- _leetCode/0004-median-of-two-sorted-arrays.js
+++ _leetCode/0004-median-of-two-sorted-arrays.js
@@ -10,10 +10,39 @@ const caseNums2_2 = [3,4];
 const caseNums1_3 = [0,0,0,0,0];
 const caseNums2_3 = [-1,0,0,0,0,0,1];
 
-if(true){
+// Basic solution but not O(log (m+n))
+// O((m + n) log (m + n))
+if(false){
     var findMedianSortedArrays = function(nums1, nums2) {
         const nums3 = nums1.concat(nums2).sort((a, b) => a - b);
         const length = nums3.length;
+
+        if (length % 2 !== 0) {
+            return nums3[Math.floor(length / 2)];                       // If odd, return the middle element
+        } else {
+            return (nums3[length / 2] + nums3[length / 2 - 1]) / 2;     // If even, return the average of two middle elements
+        }
+    };
+}
+
+// Not O(log (m+n))
+// O(m + n)
+if(true){
+    var findMedianSortedArrays = function(nums1, nums2) {
+        const nums3 = [];
+        const n1_l = nums1.length, n2_l = nums2.length;
+        let n1 = 0, n2 = 0;
+        const length = n1_l + n2_l;
+
+        for(let i = 0; i < length; i++){
+            if(n2 >= n2_l || (n1 < n1_l && nums1[n1] <= nums2[n2])){
+                nums3.push(nums1[n1]);
+                n1++;
+            } else {
+                nums3.push(nums2[n2]);
+                n2++;
+            }
+        }
 
         if (length % 2 !== 0) {
             return nums3[Math.floor(length / 2)];                       // If odd, return the middle element
