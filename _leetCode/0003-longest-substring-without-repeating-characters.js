e0f89f30168debe1e32b0fee07a4c91f29a7be3c
paulb <pabennett@hotmail.co.uk>
2024-10-15 13:06:50 +0100

    [LeetCode 0003] +

--- _leetCode/0003-longest-substring-without-repeating-characters.js
+++ _leetCode/0003-longest-substring-without-repeating-characters.js
@@ -7,8 +7,8 @@ const caseS_3 = "pwwkew";
 const caseS_4 = " ";
 const caseS_5 = "dvdf";
 
-// Increment along with 2 pointers
-if(true){
+// Increment along with 2 pointers.
+if(false){
     var lengthOfLongestSubstring = function(s) {
         if(s.length === 0){
             return 0;
@@ -23,7 +23,7 @@ if(true){
             if(!mySet.has(s[j])){
                 mySet.add(s[j]);
                 j += 1;
-                res = Math.max(res, i - j);
+                res = Math.max(res, j - i);
             } else {
                 while(s[i] !== s[j]){
                     mySet.delete(s[i]);
@@ -32,6 +32,33 @@ if(true){
 
                 mySet.delete(s[i]);
                 i += 1;
+            }
+        }
+
+        return res;
+    };
+}
+
+// Go through each character then create new substring as needed.
+if(true){
+    var lengthOfLongestSubstring = function(s) {
+        if(s.length == 0){
+            return 0;
+        }
+
+        let res = 0;
+        let curr = "";
+
+        for(k in s){
+            const c = s[k];
+            if(curr.indexOf(c) === -1){
+                curr += c;
+            } else {
+                curr = curr.slice(curr.indexOf(c) + 1) + c;
+            }
+
+            if(curr.length > res){
+                res = curr.length;
             }
         }
 
