13b00a3b5cdb53563a4ada0e41adbd418c534e31
paulb <pabennett@hotmail.co.uk>
2024-10-16 12:11:23 +0100

    [LeetCode 0009] +

--- _leetCode/0009-palindrome-number.js
+++ _leetCode/0009-palindrome-number.js
@@ -5,11 +5,18 @@ const caseNum_1 = 121;
 const caseNum_2 = -121;
 const caseNum_3 = 10;
 
-if(true){
+if(false){
     var isPalindrome = function(x) {
         const numString = String(x);
         const numStringReversed = numString.split('').reverse().join('');
         return numString === numStringReversed;
+    };
+}
+
+if(true){
+    var isPalindrome = function(x) {
+        const numString = String(x);
+        return numString === numString.split('').reverse().join('');
     };
 }
 
