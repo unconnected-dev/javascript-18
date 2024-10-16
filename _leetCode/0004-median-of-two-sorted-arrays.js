78d1a085db93118f1b9c6b8bbade1c86bad73f1e
paulb <pabennett@hotmail.co.uk>
2024-10-16 10:25:45 +0100

    [LeetCode 0004] +

--- _leetCode/0004-median-of-two-sorted-arrays.js
+++ _leetCode/0004-median-of-two-sorted-arrays.js
@@ -54,7 +54,7 @@ if(false){
 
 // Not O(log (m+n))
 // O(m + n)
-if(true){
+if(false){
     var findMedianSortedArrays = function(nums1, nums2) {
         const n1_l = nums1.length, n2_l = nums2.length;
         const length = n1_l + n2_l;
@@ -77,7 +77,61 @@ if(true){
         } else {
             return (nums3[n3_l - 1] + nums3[n3_l - 2]) / 2;
         }
-    }
+    };
+}
+
+// Looking for overlapping partitions between the two arrays.
+// O(log(min(m, n)))
+if(true){
+    var findMedianSortedArrays = function(nums1, nums2) {
+        // Ensure nums1 is the smaller array for binary search efficiency
+        if(nums1.length > nums2.length){
+            [nums1, nums2] = [nums2, nums1];
+        }
+
+        const n1_l = nums1.length, n2_l = nums2.length;
+        
+        // Set up binary search range
+        // Start of the smaller array and its end
+        let low = 0, high = n1_l;
+
+        // Continue binary search until the range is valid
+        while(low <= high){
+            // Partition index for nums1
+            const part1 = Math.floor((low + high) / 2);
+            // Partition index for nums2 calculated based on part1
+            const part2 = Math.floor((n1_l + n2_l + 1) / 2) - part1;
+
+
+            // Handle edge cases for partitions
+            const left1 = part1 === 0 ? -Infinity : nums1[part1 - 1];   // Element of left partition of nums1
+            const right1 = part1 === n1_l ? Infinity : nums1[part1];    // Element of right partition of nums1
+
+            const left2 = part2 === 0 ? -Infinity : nums2[part2 - 1];   // Element of left partition of nums2
+            const right2 = part2 === n2_l ? Infinity : nums2[part2];    // Element of right partition of nums2
+
+
+            // Check if found the correct partition
+            if(left1 <= right2 && left2 <= right1){
+
+                if ((n1_l + n2_l) % 2 === 0) {
+                    // If even, return the average of the max of lefts and min of rights
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2;
+                } else {
+                    // If odd, return the max of left partition
+                    return Math.max(left1, left2);
+                }
+            } else if (left1 > right2) {
+                // We are too far on the right side for part1. Go left.
+                // Move the high index left to decrease part1
+                high = part1 - 1;
+            } else {
+                // We are too far on the left side for part1. Go right.
+                // Move the low index right to increase part1
+                low = part1 + 1;
+            } 
+        }
+    };
 }
 
 console.log(`${findMedianSortedArrays(caseNums1_1, caseNums2_1)}`);
