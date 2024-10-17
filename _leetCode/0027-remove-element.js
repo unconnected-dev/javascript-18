ad5581401c662bdc02873fa2b76178206af8369f
paulb <pabennett@hotmail.co.uk>
2024-10-18 00:59:48 +0100

    [LeetCode 0027]

---
+++ _leetCode/0027-remove-element.js
@@ -0,0 +1,27 @@
+// Remove Element
+// https://leetcode.com/problems/remove-element/description/
+
+const caseNums_1 = [3,2,2,3];
+const caseVal_1 = 3;
+
+const caseNums_2 = [0,1,2,2,3,0,4,2];
+const caseVal_2 = 2;
+
+if(true){
+    var removeElement = function(nums, val) {
+        let i = 0;
+
+        while(i < nums.length){
+            if(nums[i] === val){
+                nums.splice(i ,1);
+                continue;
+            }
+            i += 1;
+        }
+
+        return nums;
+    };
+}
+
+console.log(`${removeElement(caseNums_1, caseVal_1)}`);
+console.log(`${removeElement(caseNums_2, caseVal_2)}`);
