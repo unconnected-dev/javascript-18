bf72b62d7a3b927f3eccefddfad47f6cba45eef2
paulb <pabennett@hotmail.co.uk>
2024-10-18 13:25:37 +0100

    [LeetCode 0027] +

--- _leetCode/0027-remove-element.js
+++ _leetCode/0027-remove-element.js
@@ -8,10 +8,11 @@ const caseNums_2 = [0,1,2,2,3,0,4,2];
 const caseVal_2 = 2;
 
 // Removes by splicing out the element
+// Doesn't complete the question due to it wanting to keep the array size
 if(false){
     var removeElement = function(nums, val) {
         let i = 0;
-
+        
         while(i < nums.length){
             if(nums[i] === val){
                 nums.splice(i ,1);
@@ -19,12 +20,13 @@ if(false){
             }
             i += 1;
         }
-
+        
         return nums;
     };
 }
 
-if(true){
+// Doesn't complete the question due to it wanting to keep the array size
+if(false){
     var removeElement = function(nums, val) {
 
         while(nums.includes(val)){
@@ -32,6 +34,23 @@ if(true){
         }
 
         return nums.length;
+    };
+}
+
+// Question requires in-place not a new array
+// Return the number of elements in nums not equal to val
+// Tracks elements equal to val via i and replaces with n
+if(true){
+    var removeElement = function(nums, val){
+        let i = 0;
+        for(n of nums){
+            if(n !== val){
+                nums[i] = n;
+                i++;
+            }
+        }
+
+        return i;
     };
 }
 
