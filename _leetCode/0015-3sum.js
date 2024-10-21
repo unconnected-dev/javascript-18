b8d78e230d90dc3a5d39b38d882a96ae02b46757
paulb <pabennett@hotmail.co.uk>
2024-10-21 12:55:26 +0100

    [LeetCode 0015] +

--- _leetCode/0015-3sum.js
+++ _leetCode/0015-3sum.js
@@ -6,7 +6,7 @@ const caseNums_2 = [0,1,1];
 const caseNums_3 = [0,0,0];
 const caseNums_4 = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0];
 
-if(true){
+if(false){
     var threeSum = function(nums) {
         let i = 0;
         nums.sort((a, b) => a - b);
@@ -46,6 +46,55 @@ if(true){
         }
         
         return Object.values(res);
+    };
+}
+
+if(true){
+    var threeSum = function(nums) {
+        nums = nums.sort((a, b) => a - b);
+        const l = nums.length;
+    
+        if (l <= 2) {
+            return [];
+        }
+    
+        const res = [];
+    
+        // Loop iterates over nums, -2 to ensure there are at least two more
+        // elements after the current nums[i]
+        for (let i = 0; i < l - 2; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                // Skip duplicate elements to avoid duplicate triplets
+                continue;
+            }
+    
+            let left = i + 1;
+            let right = l - 1;
+    
+            // This while loop checks if the sum of nums[i], nums[left] and nums[right] is zero
+            while (left < right) {
+                const total = nums[i] + nums[left] + nums[right];
+                if (total === 0) {
+                    res.push([nums[i], nums[left], nums[right]]);
+                    left++;
+                    right--;
+    
+                    // Move past duplicates
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] === nums[right + 1]) {
+                        right--;
+                    }
+                } else if (total < 0) {
+                    left++; 
+                } else {
+                    right--;
+                }
+            }
+        }
+    
+        return res;
     };
 }
 
