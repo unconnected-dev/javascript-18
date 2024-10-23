e12522672e748926a2f4a49c89304a082becda58
paulb <pabennett@hotmail.co.uk>
2024-10-23 23:22:14 +0100

    [LeetCode 0020] +

--- _leetCode/0020-valid-parentheses.js
+++ _leetCode/0020-valid-parentheses.js
@@ -9,7 +9,7 @@ const caseS_5 = "([)]";
 const caseS_6 = "]";
 const caseS_7 = "(])";
 
-if(true){
+if(false){
     var isValid = function(s) {
         const myClosedDict = {};
         myClosedDict["}"] = "{";
@@ -36,3 +36,37 @@ if(true){
         return res === "";
     };
 }
+
+if(true){
+    var isValid = function(s) {
+        const myClosedDict = {
+            "}": "{",
+            "]": "[",
+            ")": "("
+        };
+        
+        const myOpenSet = new Set(["{", "(", "["]);
+        
+        let res = "";
+        for (let c of s) {
+        
+            if (myOpenSet.has(c)) {
+                res += c;
+            } else if (res.length >= 1 && res[res.length - 1] === myClosedDict[c]) {
+                res = res.slice(0, -1);
+            } else if (c in myClosedDict) {
+                return false;
+            }
+        }
+        
+        return res === "";
+    };
+}
+
+console.log(`${isValid(caseS_1)}`);
+console.log(`${isValid(caseS_2)}`);
+console.log(`${isValid(caseS_3)}`);
+console.log(`${isValid(caseS_4)}`);
+console.log(`${isValid(caseS_5)}`);
+console.log(`${isValid(caseS_6)}`);
+console.log(`${isValid(caseS_7)}`);
