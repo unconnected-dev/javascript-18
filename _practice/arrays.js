bc2ba55d102d3def74af5440817eb06016be6d4f
paulb <pabennett@hotmail.co.uk>
2024-10-30 22:19:35 +0000

    [Practice] Arrays

--- _practice/arrays.js
+++ _practice/arrays.js
@@ -11,7 +11,27 @@ function generateBigNumberArray(){
     return arr;
 }
 
+if(true){
+
+    // default values
+    function test(a=1,b=2,c=3){
+        console.log(`a: ${a}, b: ${b}, c: ${c}`);
+    }
+
+    test(undefined, undefined, 3);
+}
+
 if(false){
+    const digits = [0,1,2,3,4,5,6,7,8,9];
+    //spreading an array
+    function spreaded(a, b, c){
+        console.log(`a: ${a}, b: ${b}, c: ${c}`);
+    }
+    spreaded(...digits);
+}
+
+if(false){
+    // get even numbers from array and push into another
     function getEvens(arr){
         const evenArr = [];
         for(let i = 0; i < arr.length; i++){
