d2d15e88ec11fbd279952e9dd7bffdc8a6207769
paulb <pabennett@hotmail.co.uk>
2024-11-21 23:48:16 +0000

    [Practice] Strings

--- _practice/strings.js
+++ _practice/strings.js
@@ -5,6 +5,15 @@
 const aString = `just some text to play with`;
 
 if(false){
+    function reverseStr(str){
+        return str.split('').reverse().join('');
+    }
+
+    const res = reverseStr(aString);
+    console.log(`res: ${res}`);
+}
+
+if(false){
     // Basic split
     const [a, b, ...c] = aString.split(' ');
 
