e1cd91512fe4b6df2916d807a5bec90ce7f02cba
paulb <pabennett@hotmail.co.uk>
2024-11-22 22:43:13 +0000

    [Practice] Strings

--- _practice/strings.js
+++ _practice/strings.js
@@ -5,6 +5,18 @@
 const aString = `just some text to play with`;
 
 if(false){
+    function capitalizeWords(str){
+        return str.toLowerCase()
+                .split(' ')
+                .map(word => word[0].toUpperCase() + word.slice(1))
+                .join(' ');
+    }
+
+    const res = capitalizeWords(aString);
+    console.log(`res: ${res}`);
+}
+
+if(false){
     function reverseStr(str){
         return str.split('').reverse().join('');
     }
