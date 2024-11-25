7f2410cb6b32402d5bf27dfd04c64b19bec9d1d7
paulb <pabennett@hotmail.co.uk>
2024-11-25 13:24:26 +0000

    [LeetCode 0026] +

--- _leetCode/0026-remove-duplicates-from-sorted-array.js
+++ _leetCode/0026-remove-duplicates-from-sorted-array.js
@@ -4,8 +4,24 @@
 const caseNums_1 = [1,1,2];
 const caseNums_2 = [0,0,1,1,1,2,2,3,3,4];
 
-// Remove duplicates by checking the next value over
+// Removes duplicates with a set
+// This is not a correct solution though
 if(true){
+    var removeDuplicates = function(nums) {
+
+        const aSet = new Set(nums);
+        nums.length = 0;
+        
+        for (const num of aSet) {
+            nums.push(num);
+        }
+        
+        return nums;
+    };
+}
+
+// Remove duplicates by checking the next value over
+if(false){
     var removeDuplicates = function(nums) {
         let i = 0;
     
